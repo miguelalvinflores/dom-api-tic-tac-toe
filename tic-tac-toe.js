d3dab604f0802ed22da4dbd527a4449cf08eef2c
Luke  Kingsley <mzootfb@Lukes-MacBook-Pro.local>
2021-02-05 18:36:46 -0500

    give up complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,80 +3,103 @@ let squareArr = ["", "", "", "", "", "", "", "", ""];
 gameStatus = "";
 
 const newGame = () => {
-    button = document.getElementById("new-game");
-    if (gameStatus !== '') {
-        button.addEventListener("click", (event) => {
-            document
-                .getElementById('game-status')
-                .innerHTML = '';
-            for (i = 0; i < 9; i++) {
-                let div = document.getElementById(`square-${i}`);
-                div.innerHTML = '';
+  let newButton = document.getElementById("new-game");
+  if (gameStatus !== "") {
+    newButton.addEventListener("click", (event) => {
+      document.getElementById("game-status").innerHTML = "";
+      for (i = 0; i < 9; i++) {
+        let div = document.getElementById(`square-${i}`);
+        div.innerHTML = "";
+      }
+      currentPlayerSymbol = "x";
+      squareArr = ["", "", "", "", "", "", "", "", ""];
+      gameStatus = "";
+      newButton.disabled = true;
+      document.getElementById("give-up").disabled = false;
+    });
+  }
+};
 
-            }
-            currentPlayerSymbol = "x";
-            squareArr = ["", "", "", "", "", "", "", "", ""];
-            gameStatus = "";
-            button.disabled = true;
-        })
+const giveUp = () => {
+  let button = document.getElementById("give-up");
+  button.addEventListener("click", (event) => {
+    if (currentPlayerSymbol === "x") {
+      gameStatus = "o";
+      document.getElementById(
+        "game-status"
+      ).innerHTML = `Winner: ${gameStatus.toUpperCase()}`;
+    } else {
+      gameStatus = "x";
+      document.getElementById(
+        "game-status"
+      ).innerHTML = `Winner: ${gameStatus.toUpperCase()}`;
     }
-}
+    button.disabled = true;
+    document.getElementById("new-game").disabled = false;
+    newGame();
+  });
+};
 
 const checkGameStatus = () => {
-// Check rows
-    for (let i = 0; i < 9; i +=3) {
-        if(squareArr[i] !== ''
-        && squareArr[i] === squareArr[i + 1]
-        && squareArr[i] === squareArr[i + 2]) {
-            gameStatus = squareArr[i];
-            break;
-        }
+  // Check rows
+  for (let i = 0; i < 9; i += 3) {
+    if (
+      squareArr[i] !== "" &&
+      squareArr[i] === squareArr[i + 1] &&
+      squareArr[i] === squareArr[i + 2]
+    ) {
+      gameStatus = squareArr[i];
+      break;
     }
-// Check columns
-    for (let i = 0; i < 3; i++) {
-        if(squareArr[i] !== ''
-        && squareArr[i] === squareArr[i + 3]
-        && squareArr[i] === squareArr[i + 6]) {
-            gameStatus = squareArr[i];
-            break;
-        }
+  }
+  // Check columns
+  for (let i = 0; i < 3; i++) {
+    if (
+      squareArr[i] !== "" &&
+      squareArr[i] === squareArr[i + 3] &&
+      squareArr[i] === squareArr[i + 6]
+    ) {
+      gameStatus = squareArr[i];
+      break;
     }
+  }
 
-//Check diagonals
-    if (squareArr[0] !== ''
-    && squareArr[0] === squareArr[4]
-    && squareArr[0] === squareArr[8]) {
-        gameStatus = squareArr[0];
+  //Check diagonals
+  if (
+    squareArr[0] !== "" &&
+    squareArr[0] === squareArr[4] &&
+    squareArr[0] === squareArr[8]
+  ) {
+    gameStatus = squareArr[0];
+  }
+  if (
+    squareArr[2] !== "" &&
+    squareArr[2] === squareArr[4] &&
+    squareArr[2] === squareArr[6]
+  ) {
+    gameStatus = squareArr[2];
+  }
+  // Check for a tie
+  let boardIsFilled = true;
+  for (let i = 0; i < 9; i++) {
+    if (squareArr[i] === "") {
+      boardIsFilled = false;
+      break;
     }
-    if (squareArr[2] !== ''
-    && squareArr[2] === squareArr[4]
-    && squareArr[2] === squareArr[6]) {
-        gameStatus = squareArr[2];
-    }
-// Check for a tie
-    let boardIsFilled = true;
-    for (let i = 0; i < 9; i++) {
-        if (squareArr[i] === '') {
-            boardIsFilled = false;
-            break;
-        }
-    }
+  }
 
-    if(boardIsFilled) {
-        gameStatus = 'None'
-    }
+  if (boardIsFilled) {
+    gameStatus = "None";
+  }
 
-    if (gameStatus !== '') {
-        document
-            .getElementById('game-status')
-            .innerHTML = `Winner: ${gameStatus.toUpperCase()}`;
+  if (gameStatus !== "") {
+    document.getElementById(
+      "game-status"
+    ).innerHTML = `Winner: ${gameStatus.toUpperCase()}`;
 
-        document
-            .getElementById("new-game")
-            .disabled = false;
-
-    }
-    newGame();
+    document.getElementById("new-game").disabled = false;
+  }
+  newGame();
 };
 
 window.addEventListener("DOMContentLoaded", (event) => {
@@ -102,6 +125,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
       currentPlayerSymbol = "x";
     }
     checkGameStatus();
-    }
-  );
+    giveUp();
+  });
 });
